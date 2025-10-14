a1bed2affcaea7d67ba208df59085b48786aa025
Gayathri <gayathripaluru2002@gmail.com>
2025-10-14 19:24:23 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,22 @@
- 
+ import React from 'react';
+import Header from './components/Header';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+import ProductDetails from './components/ProductDetails';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './styles.css';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
